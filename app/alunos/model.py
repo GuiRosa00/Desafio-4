5f7c7a89f630aa43670eb9a9cd2b4f88b59fbf20
mario jorge rosa rosa <guilherme.rosa@poli.ufrj.br>
2021-07-13 20:01:52 -0300

    Alterado json dos alunos para (horario,tipo)

--- app/alunos/model.py
+++ app/alunos/model.py
@@ -24,5 +24,5 @@ class Aluno(db.Model):
             "idade":self.idade,
             "contato":self.contato,
             "cpf":self.cpf,
-            "atividades": [atividade.horario for atividade in self.atividade]
+            "atividades": [(atividade.horario,atividade.tipo) for atividade in self.atividade]
         }
