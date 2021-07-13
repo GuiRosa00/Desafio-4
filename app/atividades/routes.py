42c882325db6850de350f392dac30f08a4b35581
mario jorge rosa rosa <guilherme.rosa@poli.ufrj.br>
2021-07-13 18:46:31 -0300

    Versão 2.0 do código

--- app/atividades/routes.py
+++ app/atividades/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from app.atividades.controller import AtividadeGeral,AtividadeID
+from app.atividades.controller import AtividadeGeral,AtividadeID,AtivaluRemove
 atividade_api = Blueprint('atividade_api',__name__)
 
 #rotas das atividades
@@ -10,4 +10,4 @@ atividade_api.add_url_rule(
          '/atividade/details/<int:id>',view_func = AtividadeID.as_view('atividade_details'),methods = ['GET','PUT','PATCH','DELETE'])
 
 atividade_api.add_url_rule(
-         '/atividade/details/<int:id>/aluno',view_func = AtividadeID.as_view('atividade_aluno'),methods = ['PATCH'])
+         '/atividade/details/<int:id>/aluno',view_func = AtivaluRemove.as_view('atividade_aluno'),methods = ['DELETE'])
