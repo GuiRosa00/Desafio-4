9c6d54bae960fdbb63ee2413a0dc18bc854c2e5f
mario jorge rosa rosa <guilherme.rosa@poli.ufrj.br>
2021-07-15 19:47:01 -0300

    Adicionado e testado nova feature envio de email

--- migrations/versions/5fbe6cae89ab_.py
+++ migrations/versions/5fbe6cae89ab_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9b30874d088b
+Revision ID: 5fbe6cae89ab
 Revises: 
-Create Date: 2021-07-15 15:02:00.854989
+Create Date: 2021-07-15 19:44:43.997428
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9b30874d088b'
+revision = '5fbe6cae89ab'
 down_revision = None
 branch_labels = None
 depends_on = None
